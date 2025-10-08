ccd385171da916c5653b7e9e6b28fcae578250f1
c-quick-watahan <c_quick@watahan.co.jp>
2025-10-08 10:35:48 +0900

    feat(middleware) Added protected pages and user upserting on login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/lib/db/prisma";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
@@ -8,6 +9,43 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }: { token: any; user: any }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (user) {
+        // Do DB work in jwt callback itself. On first call when user exists, query/create your DB user,
+        // then add that id to the token you return.
+        try {
+          const dbUser = await prisma.user.upsert({
+            where: { email: user.email },
+            update: {}, // Don't update anything if they exist
+            create: {
+              email: user.email,
+              name: user.name,
+            },
+          });
+
+          token.userId = dbUser.id;
+        } catch (error) {
+          throw error;
+        }
+      }
+      return token;
+    },
+    async session({
+      session,
+      token,
+      user,
+    }: {
+      session: any;
+      token: any;
+      user: any;
+    }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
 
 export const handler = NextAuth(authOptions);
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,23 @@
 import { NextResponse, NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/", request.url));
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  // If no token and trying to access protected route, redirect to signin
+  if (!token) {
+    return NextResponse.redirect(new URL("/api/auth/signin", request.url));
+  }
+
+  // Otherwise, allow the request
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/:path*"],
+  matcher: ["/edit-profile/:path*"],
 };
